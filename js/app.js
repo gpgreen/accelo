5292c351df639f8d75913e20a4f5550117a3cc86
Greg Green <gpgreen@gmail.com>
2014-12-31 13:53:27 -0800

    Converted to Float64 and Int32 arrays

--- js/app.js
+++ js/app.js
@@ -14,10 +14,13 @@ window.addEventListener('DOMContentLoaded', function() {
   var graph;
   var xPadding = 20;
   var yPadding = 30;
-  var verScale = 2.2;
+  var verScale = 3;
   var yPixelScale;
   var yAxis;
   var data = { 
+      maxLen:0,
+      len:0,
+      offset:0,
       values:[],
       pixels:[]
   };
@@ -27,6 +30,7 @@ window.addEventListener('DOMContentLoaded', function() {
   navigator.mozL10n.once(start);
 
   // ---
+  init();
 
   function start() {
 
@@ -36,86 +40,143 @@ window.addEventListener('DOMContentLoaded', function() {
       // https://developer.mozilla.org/Web/API/Element.innerHTML#Security_considerations
       message.textContent = translate('message');
 
-      // add event listener
-      window.addEventListener("devicemotion", handleMotion, true);
-      //console.log("listen to 'devicemotion'");
-
+  }
+  
+  function init() {
       // calculate graph scaling vars
       graph = document.getElementById('graph');
+
+      // initialize data structure
+      data.maxLen = (graph.width - xPadding) / 2;
+      data.values = new Float64Array(data.maxLen*3);
+      data.pixels = new Int32Array(data.maxLen*3);
+
       // pixels/accel val
       yPixelScale = (graph.height - yPadding) / 9.80665 / verScale;
       // location of y axis, accel = 0
       yAxis = (graph.height - yPadding) / 2;
+
+      // add event listener
+      window.addEventListener("devicemotion", handleMotion, true);
+      //console.log("listen to 'devicemotion'");
+
+      // setup the draw function
+      setInterval(draw, 100);
   }
-  
-  function getXPixel(val) {
+
+  function calcXPixel(val) {
       return val * 2 + xPadding;
   }
   
-  function getYPixel(val) {
+  function calcYPixel(val) {
       return graph.height - (yPixelScale * val) - yPadding - yAxis;
   }
   
   function addDataValues(xval,yval,zval) {
-      // the array has a maximum length
-      if(data.values.length >= (graph.width - xPadding) / 2) {
-	  data.values.shift();
-	  data.pixels.shift();
+      // add the values to the two arrays. The first array is the value array
+      // the second array is the pixel array which is used to draw the graph
+      // because this is a fixed length array, we do not add/remove array elements
+      // but have an offset to the current starting point in the array.
+      // Because the array may not be entirely filled up yet, there needs to 
+      // be logic to account for that
+
+      var startI, i;
+
+      // set the length or the starting offset
+      if(data.len === data.maxLen) {
+	  data.offset++;
+	  if(data.offset === data.maxLen)
+	      data.offset = 0;
       }
-      data.values.push({X: xval, Y: yval, Z: zval});
-      data.pixels.push({X: getYPixel(xval),
-			Y: getYPixel(yval),
-			Z: getYPixel(zval)})
+      else {
+	  data.len++;
+      }
+
+      // find the current starting array position, then add the length
+      // to get the place to insert the data
+      startI = data.offset + data.len;
+      if(startI >= data.maxLen) {
+	  startI -= data.maxLen;
+      }
+      startI *= 3;
+
+      // insert the data values
+      i = startI;
+      data.values[i++] = xval;
+      data.values[i++] = yval;
+      data.values[i] = zval;
+
+      // insert the pixel values
+      i = startI;
+      data.pixels[i++] = calcYPixel(xval);
+      data.pixels[i++] = calcYPixel(yval);
+      data.pixels[i] = calcYPixel(zval);
   }
+
+  function getPixel(index, element) {
+      var i = index + data.offset;
+      if(i >= data.maxLen)
+	  i = i - data.maxLen;
+      return data.pixels[i*3+element];
+  }
+
   function handleMotion(event) {
       // console.log('handleMotion');
       var a_x = event.accelerationIncludingGravity.x;
       var a_y = event.accelerationIncludingGravity.y;
       var a_z = event.accelerationIncludingGravity.z;
-      var t = event.interval;
+      //var t = event.interval;
       addDataValues(a_x, a_y, a_z);
-      // console.log('a=['+a_x+','+a_y+','+a_z);
+      //console.log('a=['+a_x+','+a_y+','+a_z);
       document.getElementById('a_x').textContent = a_x;
       document.getElementById('a_y').textContent = a_y;
       document.getElementById('a_z').textContent = a_z;
-      var c = graph.getContext('2d');
+  }
+
+  function draw() {
+      var ctx = graph.getContext('2d');
+
       // clear the graph background
-      c.fillStyle = "#000000";
-      c.fillRect(xPadding, 0, graph.width-xPadding, graph.height-yPadding);
+      ctx.fillStyle = "#000000";
+      ctx.fillRect(xPadding, 0, graph.width-xPadding, graph.height-yPadding);
+
       // draw the axis
-      c.lineWidth = 2;
-      c.strokeStyle = '#ffffff';
-      c.font = 'italic 8pt sans-serif';
-      c.textAlign = "center";
-      c.beginPath();
-      c.moveTo(xPadding, yAxis);
-      c.lineTo(graph.width, yAxis);
-      c.stroke();
-      // draw x line
-      c.strokeStyle = '#ff0000';
-      c.lineWidth = 1;
-      c.beginPath();
-      for(var i=0; i<data.pixels.length; i++) {
-	  //console.log('i='+i+' X='+data.pixels[i].X);
-	  c.lineTo(getXPixel(i), data.pixels[i].X);
+      ctx.lineWidth = 2;
+      ctx.strokeStyle = '#ffffff';
+      ctx.font = 'italic 8pt sans-serif';
+      ctx.textAlign = "center";
+      ctx.beginPath();
+      ctx.moveTo(xPadding, yAxis);
+      ctx.lineTo(graph.width, yAxis);
+      ctx.stroke();
+
+      // draw x graph
+      ctx.strokeStyle = '#ff0000';
+      ctx.lineWidth = 1;
+      ctx.beginPath();
+      for(var i=0; i<data.len; i++) {
+	  //console.log('i='+i+' X='+getPixel(i, 0));
+	  ctx.lineTo(calcXPixel(i), getPixel(i, 0));
       }
-      c.stroke();
-      // draw y line
-      c.strokeStyle = '#00ffff';
-      c.beginPath();
-      for(var i=0; i<data.pixels.length; i++) {
-	  //console.log('i='+i+' Y='+data.pixels[i].Y);
-	  c.lineTo(getXPixel(i), data.pixels[i].Y);
+      ctx.stroke();
+
+      // draw y graph
+      ctx.strokeStyle = '#00ffff';
+      ctx.beginPath();
+      for(var i=0; i<data.len; i++) {
+	  //console.log('i='+i+' Y='+getPixel(i, 1));
+	  ctx.lineTo(calcXPixel(i), getPixel(i, 1));
       }
-      c.stroke();
-      // draw z line
-      c.strokeStyle = '#00ff00';
-      c.beginPath();
-      for(var i=0; i<data.pixels.length; i++) {
-	  //console.log('i='+i+' Z='+data.pixels[i].Z);
-	  c.lineTo(getXPixel(i), data.pixels[i].Z);
+      ctx.stroke();
+
+      // draw z graph
+      ctx.strokeStyle = '#00ff00';
+      ctx.beginPath();
+      for(var i=0; i<data.len; i++) {
+	  //console.log('i='+i+' Z='+getPixel(i, 2));
+	  ctx.lineTo(calcXPixel(i), getPixel(i, 2));
       }
-      c.stroke();
+      ctx.stroke();
   }
 
 });
